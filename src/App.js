6df9fd9f75b0bc21ad5e6479da43b6ff92680f21
yknakamura <yuki.nakamura1@jp.ricoh.com>
2021-07-01 13:27:06 +0900

    added auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
 
 function App() {
   return (
@@ -19,8 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+	  <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
